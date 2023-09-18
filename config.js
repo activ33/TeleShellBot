64481bb9ebc7ce7bb7a7b5ca8b16ccc7f2fcf0c0
active <66892517+activ33@users.noreply.github.com>
2023-09-18 21:19:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ module.exports = {
     config:function(){
         return (
             {
-                adminUsers:[ADMIN_ID], //admin users' telegram id, should be numbers
-                botToken: 'YOUR_BOT_TOEKN', // bot token
+                adminUsers:[689061386], //admin users' telegram id, should be numbers
+                botToken: '6344846447:AAG9Yitc7Nmri61MmL5pnAWKzIOLi81AiPY', // bot token
 
             }
         );
